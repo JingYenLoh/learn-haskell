905dd2914916b7acca981107e72b63bc1c18e015
Loh Jing Yen <lohjingyen@gmail.com>
2017-12-13 18:35:08 +0800

    Remove some nonsense
    Playing around too much

--- Chap08/intermission.hs
+++ Chap08/intermission.hs
@@ -6,9 +6,5 @@ incTimes' times n = applyTimes times (+1) n
 
 applyTimes :: (Eq a, Num a) =>
               a -> (b -> b) -> b -> b
-applyTimes 0 f b = b
+applyTimes 0 _ b = b
 applyTimes n f b = f . applyTimes (n-1) f $ b
-
-main :: IO ()
-main = do
-  putStrLn . show $ applyTimes 5 (+1) 5 -- 10
