acfdc0fb270bab21e9546143d4c9dcefe0187d46
Loh Jing Yen <lohjingyen@gmail.com>
2017-12-14 09:04:07 +0800

    Filtering fun

---
+++ Chap09/Filtering.hs
@@ -0,0 +1,13 @@
+module Filtering where
+
+multiplesOfThree :: [Int]
+multiplesOfThree = filter (\x -> x `mod` 3 == 0) [1..30]
+
+multiplesCount :: Int
+multiplesCount = length $ multiplesOfThree
+
+myFilter :: String -> [String]
+myFilter str = filter f tokens
+  where f        = (\w -> not $ w `elem` articles)
+        tokens   = words str
+        articles = ["the", "a", "an"]
