8de916df29e8d08af9a7dd32e3d35d51871179ac
Jing Yen Loh <lohjingyen@gmail.com>
2018-10-11 17:01:48 +0800

    TBH don't really get the final one

--- Chap11/goats.hs
+++ Chap11/goats.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 -- goats.hs
 
 module Chap11.Goats where
 
+import Foreign.Marshal.Utils
+
 newtype Goats = Goats Int
-  deriving (Eq, Show)
+  deriving (Eq, Show, TooMany)
 
 newtype Cows = Cows Int
   deriving (Eq, Show)
+
+data Foo a = Foo (a, a)
 
 class TooMany a where
   tooMany :: a -> Bool
@@ -14,5 +21,11 @@ class TooMany a where
 instance TooMany Int where
   tooMany n = n > 42
 
-instance TooMany Goats where
-  tooMany (Goats n) = n > 43
+instance TooMany (Int, String) where
+  tooMany (n, _) = n > 42
+
+instance TooMany (Int, Int) where
+  tooMany (x, y) = x + y > 42
+
+instance (Num a, TooMany a) => TooMany (a, a) where
+  tooMany (x, y) = tooMany $ x + y
