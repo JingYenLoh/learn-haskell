13d7fa4a2525cd957aa7b71c53930538606361f4
Loh Jing Yen <lohjingyen@gmail.com>
2018-10-22 00:13:28 +0800

    Do Hutton's Razor exercise

---
+++ Chap11/HuttonsRazor.hs
@@ -0,0 +1,19 @@
+-- HuttonsRazor.hs
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+
+module Chap11.HuttonsRazor where
+
+data Expr
+  = Lit Integer
+  | Add Expr Expr
+
+eval :: Expr -> Integer
+eval (Lit x) = x
+eval (Add expr expr') = (eval expr) + (eval expr')
+
+printExpr :: Expr -> String
+printExpr (Lit x) = show x
+printExpr (Add expr expr')
+  = (printExpr expr)
+  ++ " + "
+  ++ (printExpr expr')
