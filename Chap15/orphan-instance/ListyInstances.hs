5515440c7e4d827c04cb75fe6419dd59dda0a133
Jing Yen Loh <lohjingyen@gmail.com>
2018-11-25 21:53:38 +0800

    See what orphan instances is about

---
+++ Chap15/orphan-instance/ListyInstances.hs
@@ -0,0 +1,13 @@
+-- |
+
+module ListyInstances where
+
+import Data.Monoid
+import Listy
+
+instance Semigroup (Listy a) where
+  (<>) (Listy l) (Listy l') =
+    Listy $ l <> l'
+
+instance Monoid (Listy a) where
+  mempty = Listy []
